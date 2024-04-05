6b34781bd8ac7227b9c6cbfd89678f5b93c64f0e
apiranzo <annapejo@gmail.com>
2024-04-05 13:42:59 +0200

    cursos

--- public/js/cursos/cursos.js
+++ public/js/cursos/cursos.js
@@ -12,7 +12,7 @@ function domCargado() {
     tbody = document.querySelector('#tbcursos');
     resumen = document.querySelector('#resumen');
 
-    
+
     form.addEventListener('submit', guardar);
 
     listado();
@@ -24,8 +24,8 @@ async function guardar(e) {
 
     const alumno = {
         nombre: form.nombre.value,
-        asistencia: form.asistencia.value,
-        nota: +form.nota.value
+        nota: form.nota.value,
+        assistencia: form.assistencia.value
     }
 
     if (form.id.value) {
@@ -60,40 +60,80 @@ async function listado() {
     console.table(alumnos);
 
     tbody.innerHTML = '';
+    resumen.innerHTML = '';
 
     alumnos.forEach(a => {
+        console.log(Boolean(a.nota));
         const tr = document.createElement('tr');
-        tr.innerHTML = `<th>${a.id}</th><td>${a.nombre}</td><td>${a.asistencia}</td><td>${a.nota}</td>
+        tr.innerHTML = `<th>${a.id}</th><td>${a.nombre}</td>
+                        <td>${((a.nota === "0") ? "Apte" : "No apte" )}</td>
+                        <td>${((a.assistencia === "0") ? "Presentat" : "No presentat" )}</td>
             <td>
-                <a href="javascript:formulario(${a.id})">Editar</a>
-                <a href="javascript:borrar(${a.id})">Borrar</a>
+                <a class="btn btn-primary m-1" href="javascript:formulario(${a.id})">Editar</a>
+                <a class="btn btn-danger m-1" href="javascript:borrar(${a.id})">Borrar</a>
             </td>`;
 
         tbody.appendChild(tr);
     });
 
-    const ul = document.createElement('ul');
+    const resumenbloc = document.createElement('div');
 
-    let asistidos =  alumnos.filter(a => a.asistencia == 1).length;
-    let noasistidos =  alumnos.filter(a => a.asistencia == 0).length;
+    let aptes = alumnos.filter(a => a.nota == 0).length;
+    let noaptes = alumnos.filter(a => a.nota == 1).length;
 
-    let total = alumnos.length; 
 
-    ul.innerHTML = `
-            <li>Total alumnos: ${alumnos.length}</li>
-            <li>Cantidad de alumnos asistidos: ${asistidos}</li> 
-            <li>Porcentaje asistidos: ${((asistidos / total) * 100).toFixed(2)}</li> 
-            <li>Cantidad de alumnos asistidos: ${noasistidos}</li> 
-            <li>Porcentaje NO asistidos: ${((noasistidos / total) * 100).toFixed(2)}</li> 
+    let asistidos = alumnos.filter(a => a.assistencia == 0).length;
+    let noasistidos = alumnos.filter(a => a.assistencia == 1).length;
+
+    let total = alumnos.length;
+
+    resumenbloc.innerHTML = `
+                    <div class="container px-4">
+                        <h2 class="pb-3 border-bottom text-center">Resum</h2>
+
+                        <div class="row d-flex align-items-center text-center mt-5 mb-3">
+                            <div>
+                            <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">Total alumnes</h3>
+                            <p class="display-6 fw-bold">${alumnos.length}</p>
+                            </div>
+                        </div>
+                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-2 g-4 py-5">
+                            <div class="col d-flex align-items-start text-center">
+                            <div>
+                                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">Aptes</h3>
+                                <p>${aptes}</p>
+                            </div>
+                            </div>
+                            <div class="col d-flex align-items-center text-center">
+                            <div>
+                                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">NO aptes</h3>
+                                <p>${noaptes}</p>
+                            </div>
+                            </div>
+                            <div class="col d-flex align-items-center text-center">
+                            <div>
+                                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">Presentats</h3>
+                                <p>${asistidos} alumnes - ${((asistidos / total) * 100).toFixed(2)}%</p>
+                            </div>
+                            </div>
+                            <div class="col d-flex align-items-center text-center">
+                            <div>
+                                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">NO presentats</h3>
+                                <p>${noasistidos} alumnes - ${((noasistidos / total) * 100).toFixed(2)}%</p>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
 
     `;
 
-    resumen.appendChild(ul);    
+    resumen.appendChild(resumenbloc);
 }
 
 async function formulario(id) {
     form.style.display = null;
     table.style.display = 'none';
+    resumen.style.display = 'none';
 
     if (id) {
         const respuesta = await fetch(URL + id);
@@ -107,7 +147,7 @@ async function formulario(id) {
         form.reset();
     }
 
-    
+
 }
 
 function borrar(id) {
