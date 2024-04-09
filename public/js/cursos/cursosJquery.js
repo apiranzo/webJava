33280ee87014b02ba605278bd3abf28b888af5bf
apiranzo <annapejo@gmail.com>
2024-04-09 13:39:45 +0200

    resum cursos

--- public/js/cursos/cursosJquery.js
+++ public/js/cursos/cursosJquery.js
@@ -59,57 +59,64 @@ function listado() {
                 </tr>
             `).appendTo($('tbody'));
         });
+        const resumenbloc = document.createElement('div');
+    
+        let aptes = alumnos.filter(a => a.nota == 0).length;
+        let noaptes = alumnos.filter(a => a.nota == 1).length;
+    
+    
+        let asistidos = alumnos.filter(a => a.assistencia == 0).length;
+        let noasistidos = alumnos.filter(a => a.assistencia == 1).length;
+    
+        let total = alumnos.length;
+
+        let blockresum = `<div class="container px-4">
+        <h2 class="pb-3 border-bottom text-center">Resum</h2>
+
+        <div class="row d-flex align-items-center text-center mt-5 mb-3">
+            <div>
+            <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">Total alumnes</h3>
+            <p class="display-6 fw-bold">${alumnos.length}</p>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-2 g-4 py-5">
+            <div class="col d-flex align-items-start text-center">
+            <div>
+                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">Aptes</h3>
+                <p>${aptes}</p>
+            </div>
+            </div>
+            <div class="col d-flex align-items-center text-center">
+            <div>
+                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">NO aptes</h3>
+                <p>${noaptes}</p>
+            </div>
+            </div>
+            <div class="col d-flex align-items-center text-center">
+            <div>
+                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">Presentats</h3>
+                <p>${asistidos} alumnes - ${((asistidos / total) * 100).toFixed(2)}%</p>
+            </div>
+            </div>
+            <div class="col d-flex align-items-center text-center">
+            <div>
+                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">NO presentats</h3>
+                <p>${noasistidos} alumnes - ${((noasistidos / total) * 100).toFixed(2)}%</p>
+            </div>
+            </div>
+        </div>
+    </div>
+
+`
+        if (total == alumnos.length){
+            $('#sec-resum').empty();
+            $(blockresum).appendTo($('#sec-resum'));
+        } else {
+            $('#sec-resum').empty();
+            $(blockresum).appendTo($('#sec-resum'));
+        }
     });
 
-    const resumenbloc = document.createElement('div');
-
-    let aptes = alumnos.filter(a => a.nota == 0).length;
-    let noaptes = alumnos.filter(a => a.nota == 1).length;
-
-
-    let asistidos = alumnos.filter(a => a.assistencia == 0).length;
-    let noasistidos = alumnos.filter(a => a.assistencia == 1).length;
-
-    let total = alumnos.length;
-
-    $(`<div class="container px-4">
-                        <h2 class="pb-3 border-bottom text-center">Resum</h2>
-
-                        <div class="row d-flex align-items-center text-center mt-5 mb-3">
-                            <div>
-                            <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">Total alumnes</h3>
-                            <p class="display-6 fw-bold">${alumnos.length}</p>
-                            </div>
-                        </div>
-                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-2 g-4 py-5">
-                            <div class="col d-flex align-items-start text-center">
-                            <div>
-                                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">Aptes</h3>
-                                <p>${aptes}</p>
-                            </div>
-                            </div>
-                            <div class="col d-flex align-items-center text-center">
-                            <div>
-                                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">NO aptes</h3>
-                                <p>${noaptes}</p>
-                            </div>
-                            </div>
-                            <div class="col d-flex align-items-center text-center">
-                            <div>
-                                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">Presentats</h3>
-                                <p>${asistidos} alumnes - ${((asistidos / total) * 100).toFixed(2)}%</p>
-                            </div>
-                            </div>
-                            <div class="col d-flex align-items-center text-center">
-                            <div>
-                                <h3 class="fw-bold mb-3 fs-4 text-body-emphasis">NO presentats</h3>
-                                <p>${noasistidos} alumnes - ${((noasistidos / total) * 100).toFixed(2)}%</p>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-    
-    `).appendTo($('#sec-resum'));
 
 }
 
